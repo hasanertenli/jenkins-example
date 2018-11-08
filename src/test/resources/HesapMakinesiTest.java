3a1e051e18dc929e60ecb946b0cef9f8a8acd958
example <hasanertenli13@gmail.com>
2018-11-08 17:34:49 +0300

    jUnit denemesi

---
+++ src/test/resources/HesapMakinesiTest.java
@@ -0,0 +1,18 @@
+package com.example.jenkinsexample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HesapMakinesiTest {
+    @Test
+    public void testTopla()
+    {
+        HesapMakinesi hesapMakinesi = new HesapMakinesi();
+        int sayi1 = 5;
+        int sayi2 = 19;
+
+        int toplam = hesapMakinesi.topla(sayi1,sayi2);
+
+        Assert.assertEquals(23, toplam);
+    }
+}
